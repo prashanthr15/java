e963d779269a6d4fae34ead3e582ad95eb8edee8
prashanthr15 <prashanthreddy.gopidi@gmail.com>
2024-01-24 21:17:29 -0600

    my first commit

---
+++ Area.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.util.Scanner;
+public class Area { 
+    public static void main(String[] args) {
+        int a,b,c;
+        double s,area;
+        Scanner scan=new Scanner(System.in);
+        System.out.print("enter the three sides:");
+        a = scan.nextInt();
+        b = scan.nextInt();
+        c = scan.nextInt();
+        s = (a+b+c)/2;
+        area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
+        System.err.println("Area of Traiangle" +area);
+     }
+}
